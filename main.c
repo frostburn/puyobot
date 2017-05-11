83d82bf62b94e65e530a6edb5ed76b8f52323204
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2017-05-11 22:18:29 +0300

    Implement gravity

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WIDTH (6)
 #define HEIGTH (10)
 #define H_SHIFT (1)
 #define V_SHIFT (6)
-#define NORTH_WALL (0x3f)
-#define WEST_WALL (0x1041041)
-#define EAST_WALL (0x20820820)
-#define WEST_BLOCK (0x1f7df7df)
+#define FULL (0xfffffffffffffffULL)
+#define BOTTOM (0xfc0000000000000ULL)
 
 #define NUM_FLOORS (2)
+#define TOTAL_HEIGHT (20)
 #define NUM_COLORS (6)
 #define RED (0)
 #define GREEN (1)
@@ -27,6 +28,14 @@ typedef struct state
     puyos_t floors[NUM_FLOORS][NUM_COLORS];
     int chain;
 } state;
+
+int popcount(puyos_t puyos) {
+    return __builtin_popcountll(puyos);
+}
+
+puyos_t lrand() {
+    return rand() | (((puyos_t) rand()) << 30) | (((puyos_t) rand()) << 60);
+}
 
 void print_puyos(puyos_t puyos) {
     printf(" ");
@@ -93,15 +102,93 @@ void print_state(state *s) {
     printf("chain=%d\n", s->chain);
 }
 
-int main() {
-    print_puyos(7018292150115006956ULL);
+// Reference for bit parallel gravity
+puyos_t drop_once(puyos_t puyos) {
+    puyos_t line = BOTTOM;
+    for (int i = 0; i < HEIGTH; ++i) {
+        puyos_t above = (puyos << V_SHIFT) & line;
+        line >>= V_SHIFT;
+        puyos = (puyos & ~line) | above | ((above & puyos) >> V_SHIFT);
+    }
+    return puyos;
+}
+
+void handle_gravity(state *s) {
+    puyos_t all[2];
+    for (int i = 0; i < NUM_FLOORS; ++i) {
+        all[i] = 0;
+        for (int j = 0; j < NUM_COLORS; ++j) {
+            all[i] |= s->floors[i][j];
+        }
+    }
+    for (int i = 0; i < WIDTH; ++i) {
+        int pile_size = 0;
+        for (int j = 0; j < TOTAL_HEIGHT; ++j) {
+            int scanner_floor = 1;
+            int scanner_y = HEIGTH - 1 - j;
+            if (j >= HEIGTH) {
+                scanner_floor = 0;
+                scanner_y = 2 * HEIGTH - 1 - j;
+            }
+            puyos_t scanner = 1ULL << (scanner_y * WIDTH + i);
+            if (!(scanner & all[scanner_floor])) {
+                continue;
+            }
+            for (int k = 0; k < NUM_COLORS; ++k) {
+                if (scanner & s->floors[scanner_floor][k]) {
+                    s->floors[scanner_floor][k] ^= scanner;
+                    int pile_floor = 1;
+                    int pile_y = HEIGTH - 1 - pile_size;
+                    if (pile_size >= HEIGTH) {
+                        pile_floor = 0;
+                        pile_y = 2 * HEIGTH - 1 - pile_size;
+                    }
+                    s->floors[pile_floor][k] |= 1ULL << (pile_y * WIDTH + i);
+                    ++pile_size;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void test_lrand() {
+    srand(time(NULL));
+    puyos_t result = 0;
+    for (int i = 0; i < 10000; ++i) {
+        result |= lrand();
+    }
+    assert(result == ~0);
+}
+
+void test_gravity() {
     state *s = calloc(1, sizeof(state));
-    s->floors[0][0] = 3;
-    s->floors[0][1] = 4;
-    s->floors[1][2] = 1;
-    s->floors[1][3] = 2;
-    s->floors[1][4] = 8;
-    s->floors[0][5] = 128;
+    srand(time(NULL));
+    int puyo_count = 0;
+    for (int j = 0; j < NUM_FLOORS; j++) {
+        puyos_t allowed = FULL;
+        for (int i = 0; i < NUM_COLORS; ++i) {
+            puyos_t candidates = lrand() & lrand() & lrand();
+            candidates &= allowed;
+            allowed ^= candidates;
+            s->floors[j][i] = candidates;
+            puyo_count += popcount(candidates);
+        }
+    }
     print_state(s);
+    handle_gravity(s);
+    int new_puyo_count = 0;
+    for (int j = 0; j < NUM_FLOORS; j++) {
+        for (int i = 0; i < NUM_COLORS; ++i) {
+            new_puyo_count += popcount(s->floors[j][i]);
+        }
+    }
+    print_state(s);
+    assert(puyo_count == new_puyo_count);
+}
+
+int main() {
+    test_lrand();
+    test_gravity();
     return 0;
 }
