677e79e9c1417c9cb677b76bf7481eba2cc4576a
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2017-05-25 22:48:35 +0300

    Allow initial states and no visuals for policy demos

--- demo.c
+++ demo.c
@@ -13,15 +13,16 @@ void print_deals(content_t *deals, int num_deals) {
     printf("\x1b[0m\033[B\033[B\n");
 }
 
-void policy_demo(int do_animation, size_t iterations, policy_fun policy) {
-    state *s = calloc(1, sizeof(state));
+void policy_demo(state *s, int do_animation, size_t iterations, policy_fun policy) {
     content_t deals[NUM_DEALS];
     int total_score = 0;
     double puyos_played = 0;
     int game_overs = 0;
     int max_chain = 0;
 
-    print_state(s);
+    if (do_animation >= 0) {
+        print_state(s);
+    }
     void cb(state *s) {
         redraw_state(s);
         print_deals(deals, 3);
@@ -39,9 +40,9 @@ void policy_demo(int do_animation, size_t iterations, policy_fun policy) {
             clear_state(s);
         }
         state *c = copy_state(s);
-        if (do_animation) {
+        if (do_animation == 1) {
             animate(c, cb);
-        } else {
+        } else if (do_animation == 0) {
             cb(c);
         }
         free(c);
@@ -60,56 +61,10 @@ void policy_demo(int do_animation, size_t iterations, policy_fun policy) {
     printf("\nDone\n");
 }
 
-void demo(int do_animation, size_t iterations, eval_fun f, float tree_value_multiplier) {
+void eval_demo(int do_animation, size_t iterations, eval_fun f, float tree_value_multiplier) {
     content_t policy(state *s, content_t *deals, size_t num_deals) {
         return solve(s, deals, num_deals, 0, f, tree_value_multiplier);
     }
-    policy_demo(do_animation, iterations, policy);
-}
-
-// TODO: Convert into a policy for the policy demo.
-void mc_demo(int do_animation, size_t iterations, policy_fun policy) {
     state *s = calloc(1, sizeof(state));
-    content_t deals[3];
-    int max_chain = 0;
-    int total_score = 0;
-    double puyos_played = 0;
-
-    for (int i = 0; i < 3; ++i) {
-        deals[i] = rand_piece();
-    }
-
-    for (int i = 0; i < 10000; ++i) {
-        content_t choice = iterate_mc(s, deals, 3, iterations, policy);
-        if (!apply_deal_and_choice(s, deals[0], choice)) {
-            print_state(s);
-            printf("Game Over\n");
-            return;
-        }
-
-        void cb(state *s) {
-            print_state(s);
-            print_deals(deals, 3);
-            printf("score=%d, max chain=%d, efficiency=%f\n", total_score, max_chain, total_score / puyos_played);
-        }
-
-        state *c = copy_state(s);
-        if (do_animation) {
-            animate(c, cb);
-        } else {
-            cb(c);
-        }
-        free(c);
-        int chain = 0;
-        int score = resolve(s, &chain);
-        total_score += score;
-        puyos_played += 2;
-        if (chain > max_chain) {
-            max_chain = chain;
-        }
-        for (int j = 0; j < 2; ++j) {
-            deals[j] = deals[j + 1];
-        }
-        deals[2] = rand_piece();
-    }
+    policy_demo(s, do_animation, iterations, policy);
 }
