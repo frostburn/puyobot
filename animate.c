ea3866e13b9b576a836931bb9707c037cc62422f
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2017-05-31 18:56:05 +0300

    Add animation support for bottom chains

--- animate.c
+++ animate.c
@@ -28,7 +28,7 @@ void redraw_state(state *s) {
                     } else {
                         printf(" ●");
                     }
-                    any  = 1;
+                    any = 1;
                     break;
                 }
             }
@@ -76,19 +76,83 @@ void animate_gravity(state *s, void (*callback)(state*)) {
     } while (temp[0] != all[0] || temp[1] != all[1]);
 }
 
-int animate(state *s, void (*callback)(state*)) {
-    int chain = -1;
-    int total_score = 0;
+void animate(state *s, void (*callback)(state*)) {
     while(1) {
-        ++chain;
         animate_gravity(s, callback);
         kill_puyos(s);
-        int score = clear_groups(s, chain);
-        if (!score) {
+        if (!clear_groups(s, 1)) {
             break;
         }
         usleep(50 * ANIM_DELAY);
-        total_score += score;
     }
-    return chain;
+}
+
+void redraw_bottom_spam(puyos_t *floor, int num_links, int num_colors) {
+    for (int i = 0; i <= HEIGHT; ++i) {
+        printf("\033[A");
+    }
+    printf("\n");
+    for (int i = 0; i < HEIGHT * WIDTH; ++i) {
+        if (i % V_SHIFT == 0) {
+            printf("%d", i / V_SHIFT);
+        }
+        puyos_t p = (1ULL << i);
+        int any = 0;
+        for (int k = 0; k < num_colors; ++k) {
+            if (p & floor[k]) {
+                printf("\x1b[3%d;1m", k % 6 + 1);
+                if (k < num_links) {
+                    printf(" %c", color_label(k));
+                } else {
+                    printf(" @");
+                }
+                any  = 1;
+                break;
+            }
+        }
+        printf("\x1b[0m");
+        if (!any) {
+            printf("  ");
+        }
+        if (i % V_SHIFT == V_SHIFT - 1) {
+            printf(" \n");
+        }
+    }
+}
+
+int animate_bottom_gravity(puyos_t *floor, int num_links, int num_colors, void (*callback)(puyos_t*, int, int)) {
+    puyos_t all;
+    all = 0;
+    for (int j = 0; j < num_colors; ++j) {
+        all |= floor[j];
+    }
+
+    int iterations = 0;
+    puyos_t temp;
+    do {
+        temp = all;
+        puyos_t bellow, falling;
+        bellow = (all >> V_SHIFT) | BOTTOM;
+        all = 0;
+        for (int i = 0; i < num_colors; ++i) {
+            falling = floor[i] & ~bellow;
+            floor[i] = (falling << V_SHIFT) | (floor[i] & ~falling);
+            all |= floor[i];
+        }
+        ++iterations;
+        callback(floor, num_links, num_colors);
+        usleep(ANIM_DELAY);
+    } while (temp != all);
+    return iterations;
+}
+
+void animate_bottom(puyos_t *floor, int num_links, int num_colors, void (*callback)(puyos_t*, int, int)) {
+    while(1) {
+        animate_bottom_gravity(floor, num_links, num_colors, callback);
+        int dummy;
+        if (!clear_bottom_groups(floor, num_colors, &dummy)) {
+            break;
+        }
+        usleep(50 * ANIM_DELAY);
+    }
 }
