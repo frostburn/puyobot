b93945bd322810c7c8d570b97c54da42b26abe45
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2017-06-08 22:03:25 +0300

    Implement user interface to play against a bot

--- ui/main.c
+++ ui/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <locale.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,6 +13,10 @@
 #include "../scoring.c"
 #include "../state.c"
 #include "../deal.c"
+#include "../multiplayer.c"
+
+#include "../eval.c"
+#include "../tree.c"
 
 #include "state.c"
 
@@ -30,8 +35,8 @@ void init_colors() {
 
 void play_endless() {
     WINDOW *state_win = newwin(2 * HEIGHT + 2, 2 * WIDTH + 3, 0, 0);
-    WINDOW *deal_win = newwin(4, 8, 0, 2 * WIDTH + 5);
-    WINDOW *score_win = newwin(4, 30, 4, 2 * WIDTH + 5);
+    WINDOW *deal_win = newwin(7, 3, 0, 2 * WIDTH + 5);
+    WINDOW *score_win = newwin(4, 30, 7, 2 * WIDTH + 5);
     box(state_win, 0, 0);
     box(deal_win, 0, 0);
     box(score_win, 0, 0);
@@ -55,9 +60,9 @@ void play_endless() {
         }
         deals[NUM_DEALS - 1] = rand_piece();
         content_t deal = deals[0];
-        int not_done = 1;
+        int choosing = 1;
         manipulate_piece:
-        while (not_done) {
+        while (choosing) {
             wmove(score_win, 1, 1);
             wprintw(score_win, "Score=%d", score);
             wmove(score_win, 2, 1);
@@ -79,7 +84,7 @@ void play_endless() {
                     ++x;
                     break;
                 case KEY_DOWN:
-                    not_done = 0;
+                    choosing = 0;
                     break;
                 case 'f':
                     --orientation;
@@ -88,7 +93,7 @@ void play_endless() {
                     ++orientation;
                     break;
                 case 'q':
-                    not_done = 0;
+                    choosing = 0;
                     game_running = 0;
                     break;
             }
@@ -99,7 +104,7 @@ void play_endless() {
         }
         if (!apply_deal_and_choice(s, deal, choice)) {
             clear_deal_and_choice(s);
-            not_done = 1;
+            choosing = 1;
             goto manipulate_piece;
         }
         int chain = 0;
@@ -110,9 +115,151 @@ void play_endless() {
             last_chain = chain;
         }
     }
+    free(s);
     delwin(state_win);
     delwin(score_win);
     delwin(deal_win);
+}
+
+content_t mc_practice_policy(void *pg, content_t *deals, int num_deals) {
+    mc_options options = simple_mc_options(10000, random_policy);
+    options.step = step_practice;
+    options.copy = copy_practice;
+    return iterate_mc(pg, deals, num_deals, options);
+}
+
+void play_multiplayer() {
+    WINDOW *left_player_win = newwin(2 * HEIGHT + 2, 2 * WIDTH + 3, 0, 0);
+    WINDOW *right_player_win = newwin(2 * HEIGHT + 2, 2 * WIDTH + 3, 0, 2 * WIDTH + 15);
+    WINDOW *left_deal_win = newwin(7, 3, 0, 2 * WIDTH + 5);
+    WINDOW *right_deal_win = newwin(7, 3, 0, 2 * WIDTH + 10);
+    WINDOW *left_status_win = newwin(9, 2 * WIDTH + 10, 2 * HEIGHT + 3, 0);
+    WINDOW *right_status_win = newwin(9, 2 * WIDTH + 10, 2 * HEIGHT + 3, 2 * WIDTH + 15);
+    box(left_player_win, 0, 0);
+    box(right_player_win, 0, 0);
+    box(left_deal_win, 0, 0);
+    box(right_deal_win, 0, 0);
+    box(left_status_win, 0, 0);
+    box(right_status_win, 0, 0);
+    refresh();
+
+    game *g = new_game(2, NUM_DEALS);
+    content_t choices[2];
+
+    wprint_state(left_player_win, &g->players[0].state);
+    wprint_state(right_player_win, &g->players[1].state);
+    wrefresh(left_player_win);
+    wrefresh(right_player_win);
+
+    int x = 0;
+    int orientation = 0;
+    content_t choice = 0;
+    int game_running = 1;
+    state *pop_previews[2] = {0};
+    while (game_running) {
+        practice_game *pg;
+        preview_deals(left_deal_win, g->deals + 1 + g->players[0].deal_index, g->num_deals - 1);
+        preview_deals(right_deal_win, g->deals + 1 + g->players[1].deal_index, g->num_deals - 1);
+        if (pop_previews[1]) {
+            wprint_state(right_player_win, pop_previews[1]);
+            free(pop_previews[1]);
+        }
+        wprint_game_status(left_status_win, g, 0);
+        box(left_status_win, 0, 0);
+        wprint_game_status(right_status_win, g, 1);
+        box(right_status_win, 0, 0);
+
+        pg = game_as_practice(g, 1);
+        if (pg) {
+            // choices[1] = mc_practice_policy(pg, pg->deals, pg->num_deals);  // monte carlo alternative
+            choices[1] = gcn_practice_policy(pg, pg->deals, pg->num_deals);
+            preview_deal_and_choice(right_player_win, &pg->player.state, pg->deals[0], choices[1]);
+            free(pg);
+        }
+
+        wrefresh(right_player_win);
+        wrefresh(left_deal_win);
+        wrefresh(right_deal_win);
+        wrefresh(left_status_win);
+        wrefresh(right_status_win);
+
+        int choosing = 1;
+        content_t deal = g->deals[g->players[0].deal_index];
+        pg = game_as_practice(g, 0);
+        state *s = NULL;
+        if (pg) {
+            s = &pg->player.state;
+        } else {
+            if (pop_previews[0]) {
+                // Apply the here deal to preview it too.
+                apply_deal_and_choice(pop_previews[0], deal, choice);
+                wprint_state(left_player_win, pop_previews[0]);
+                free(pop_previews[0]);
+            }
+            wrefresh(left_player_win);
+            choosing = 0;
+            getch();
+        }
+        manipulate_piece:
+        while (choosing) {
+            preview_deal_and_choice(left_player_win, s, deal, choice);
+            wrefresh(left_player_win);
+            int ch = getch();
+            switch (ch)
+            {
+                case KEY_LEFT:
+                    --x;
+                    break;
+                case KEY_RIGHT:
+                    ++x;
+                    break;
+                case KEY_DOWN:
+                    choosing = 0;
+                    break;
+                case 'f':
+                    --orientation;
+                    break;
+                case 'd':
+                    ++orientation;
+                    break;
+                case 'q':
+                    choosing = 0;
+                    game_running = 0;
+                    break;
+            }
+            choice = make_choice(&x, &orientation);
+        }
+        if (!game_running) {
+            break;
+        }
+        if (s && !state_is_full(s) && !apply_deal_and_choice(s, deal, choice)) {
+            clear_deal_and_choice(s);
+            choosing = 1;
+            goto manipulate_piece;
+        }
+        choices[0] = choice;
+        free(pg);
+
+        for (int i = 0; i < g->num_players; ++i) {
+            pg = game_as_practice(g, i);
+            if (pg) {
+                apply_deal_and_choice(&pg->player.state, g->deals[g->players[i].deal_index], choices[i]);
+                pop_previews[i] = pop_preview(&pg->player.state);
+                free(pg);
+            }
+            else {
+                pop_previews[i] = pop_preview(&g->players[i].state);
+            }
+        }
+        step_game(g, choices);
+    }
+    delwin(left_player_win);
+    delwin(right_player_win);
+    delwin(left_deal_win);
+    delwin(right_deal_win);
+    delwin(left_status_win);
+    delwin(right_status_win);
+    free(g);
 }
 
 void main() {
@@ -125,7 +272,8 @@ void main() {
     curs_set(0);
     init_colors();
 
-    play_endless();
+    // play_endless();
+    play_multiplayer();
 
     endwin();
 }
--- ui/state.c
+++ ui/state.c
@@ -49,6 +49,7 @@ void wprint_state(WINDOW *win, state *s) {
             }
         }
     }
+    wmove(win, HEIGHT * 2 + 1, 0);
 }
 
 void preview_deal_and_choice(WINDOW *win, state *s, content_t deal, content_t choice) {
@@ -63,24 +64,82 @@ void preview_deal_and_choice(WINDOW *win, state *s, content_t deal, content_t ch
         }
     }
     apply_deal_and_choice(c, deal, choice);
+    for (int j = 0; j < NUM_COLORS - 1; ++j) {
+        puyos_t piece = c->floors[0][j] & (TOP | (TOP << V_SHIFT));
+        c->floors[0][j] ^= piece;
+        c->floors[0][j] |= piece << ((GHOST_Y - 4) * V_SHIFT);
+    }
     wprint_state(win, c);
     free(c);
 }
 
 void preview_deals(WINDOW *win, content_t *deals, int num_deals) {
     wattron(win, A_BOLD);
+    int y = 1;
     for (int i = 0; i < num_deals; ++i) {
-        wmove(win, 1, 2 + 3*i);
+        wmove(win, y, 1);
         content_t color = deal_color1(deals[i]);
         wattron(win, COLOR_PAIR(color + 1));
         wprintw(win, "0");
         wattroff(win, COLOR_PAIR(color + 1));
-    }
-    for (int i = 0; i < num_deals; ++i) {
-        wmove(win, 2, 2 +  3*i);
-        content_t color = deal_color2(deals[i]);
+        ++y;
+        wmove(win, y, 1);
+        color = deal_color2(deals[i]);
         wattron(win, COLOR_PAIR(color + 1));
         wprintw(win, "0");
         wattroff(win, COLOR_PAIR(color + 1));
+        y += 2;
     }
+    wattroff(win, A_BOLD);
+}
+
+void wprint_player(WINDOW *win, player *p) {
+    wprint_state(win, &p->state);
+    double sending = (p->chain_score + p->leftover_score) / (double)TARGET_SCORE;
+    wprintw(win,
+        "score=%d\nchain=%d\nsending=%.2f\nreceiving=%d\nall clear=%d\ngame overs=%d\n",
+        p->total_score, p->chain, sending, p->pending_nuisance, p->all_clear_bonus, p->game_overs
+    );
+}
+
+void wprint_game_status(WINDOW *win, game *g, int player_index) {
+    practice_game *pg = game_as_practice(g, player_index);
+    player *p;
+    int incoming = -1;
+    int delay = 0;
+    if (pg) {
+        p = &pg->player;
+        incoming = pg->incoming;
+        delay = pg->delay;
+        free(pg);
+    } else {
+        p = g->players + player_index;
+    }
+    double sending = (p->chain_score + p->leftover_score) / (double)TARGET_SCORE;
+    wmove(win, 1, 1);
+    wprintw(win,
+        "score=%d\n chain=%d\n sending=%.2f\n receiving=%d\n all clear=%d\n game overs=%d\n",
+        p->total_score, p->chain, sending, p->pending_nuisance, p->all_clear_bonus, p->game_overs
+    );
+    if (incoming >= 0) {
+        wprintw(win, " incoming=%d/%d\n", incoming, delay);
+    } else {
+        wprintw(win, " incoming=n/a\n");
+    }
+}
+
+state* pop_preview(state *s) {
+    puyos_t all[2];
+    state *c = copy_state(s);
+    handle_gravity(c);
+    state *cc = copy_state(c);
+    clear_groups(cc, 0);
+    get_state_mask(cc, all);
+    free(cc);
+    for (int i = 0; i < NUM_FLOORS; ++i) {
+        for (int j = 0; j < NUM_COLORS - 1; ++j) {
+            c->floors[i][GARBAGE] |= c->floors[i][j] & ~all[i];
+        }
+    }
+    return c;
 }
